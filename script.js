0dd39bf42fc7bca6fe97c8cf6e17097292c8bf0a
Laura <laurastanton1@gmail.com>
2022-06-18 21:32:52 -0400

    Add function for one round of play

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function computerPlay(){
     let item = items[Math.floor(Math.random()*items.length)];
     return item;
 }
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    
+    if(playerSelection === computerSelection){
+        console.log(`You tie! You both played ${computerSelection}`);
+    }else if((playerSelection == 'Rock' && computerSelection == 'Paper') 
+            || (playerSelection == 'Paper' && computerSelection == 'Scissors')
+            || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    }else{
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    }
+}
