b157fa5c7316654c83b7a3fae7358783044d3c50
Laura <laurastanton1@gmail.com>
2022-06-18 22:04:32 -0400

    Add function for playing 5 rounds keeping track of score

--- script.js
+++ script.js
@@ -7,14 +7,47 @@ function computerPlay(){
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    
+    let result;
+
     if(playerSelection === computerSelection){
-        console.log(`You tie! You both played ${computerSelection}`);
+        console.log(`You tie! You both played ${computerSelection.toLowerCase()}`);
+        return result = 'Tie';
     }else if((playerSelection == 'Rock' && computerSelection == 'Paper') 
             || (playerSelection == 'Paper' && computerSelection == 'Scissors')
             || (playerSelection == 'Scissors' && computerSelection == 'Rock')){
-        console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        console.log(`You lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`);
+        return result = 'Lose';
     }else{
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        console.log(`You win! ${playerSelection} beats ${computerSelection.toLowerCase()}`);
+        return result = 'Win';
     }
 }
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for(let i = 0; i < 5; i++){
+        const playerSelection = prompt('Enter rock, paper, or scissors:');
+        const computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+        if(result=='Tie'){
+            continue;
+        }
+        if(result=='Lose'){
+            computerScore++;
+        }else{
+            playerScore++;
+        }
+        console.log(playerScore);
+        console.log(computerScore);
+    }
+    if(playerScore>computerScore){
+        console.log('You win!');
+    }else if(playerScore<computerScore){
+        console.log('Computer wins!');
+    }else{
+        console.log('You tie!');
+    }
+}
+
+game();
